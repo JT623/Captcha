a3b52edbe8a6db1440cd6be360614ee7e30fb000
JT623 <57306722+JT623@users.noreply.github.com>
2022-06-02 14:47:16 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,81 +1,61 @@
-import os
 import torch
 import torch.nn as nn
 from Testmodel import CNN
 from datasets import CaptchaData
 from torchvision.transforms import Compose, ToTensor
 import pandas as pd
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
 
 model_path = './checkpoints/model.pth'
 testpath = './data/test/'
-result_path = '/result/submission.csv'
+result_path = './result/submission.csv'
 
 source = [str(i) for i in range(0, 10)]
 source += [chr(i) for i in range(97, 97+26)]
 source += [chr(i) for  i in range(65,65+26)]
 alphabet = ''.join(source)
 
-def predict(img_dir=testpath):
-    transforms = Compose([ToTensor()])
-    dataset = CaptchaData(img_dir, transform=transforms)
-   # dataset = MyDataSet()
-    cnn = CNN()
-    if torch.cuda.is_available():
-        cnn = cnn.cuda()
-        cnn.eval()
-        cnn.load_state_dict(torch.load(model_path))
-    else:
-        cnn.eval()
-        model = torch.load(model_path, map_location='cpu')
-        cnn.load_state_dict(model)
-
-    for k, (img, target) in enumerate(dataset):
-        #img = img.view(1, 3, 40, 120).cuda()
-        #target = target.view(1, 4*62).cuda()
-        #target = target.view(1, 4*62).cuda()
-        img = img.view(1, 3, 40, 120)
-        output = cnn(img)
-
+def run():
+    img_list = os.listdir(testpath)
+    img_list.sort(key=lambda x:int(x.split('.')[0]))
+    img_li = list()
+    label_li = list()
+    # print(img_list)
+    for item in img_list:
+        imgpath = os.path.join(testpath,item)
+        img = Image.open(imgpath)
+        trans = ToTensor()
+        img_tensor = trans(img)
+        cnn = CNN()
+        if torch.cuda.is_available():
+            cnn = cnn.cuda()
+            cnn.eval()
+            cnn.load_state_dict(torch.load(model_path))
+        else:
+            cnn.eval()
+            model = torch.load(model_path, map_location='cpu')
+            cnn.load_state_dict(model)
+        img_tensor = img_tensor.view(1, 3, 40, 120)
+        output = cnn(img_tensor)
         output = output.view(-1, 62)
-        target = target.view(-1, 62)
         output = nn.functional.softmax(output, dim=1)
         output = torch.argmax(output, dim=1)
-        target = torch.argmax(target, dim=1)
         output = output.view(-1, 4)[0]
-        #target = target.view(-1, 4)[0]
-        a=''.join([alphabet[i] for i in output.cpu().numpy()])
-        b =''.join([alphabet[i] for i in target.cpu().numpy()])+'.jpg'
+        label = ''.join([alphabet[i] for i in output.cpu().numpy()])
+        id = "".join(item)
+        print("ID:",id , "label:",label)
+        img_li.append(img)
+        label_li.append(label)
+    for i in range(1,11):
+        plt.subplot(5,2,i)
+        plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.15, hspace=1.5)
+        plt.imshow(img_li[i-1])
+        plt.title("predict result:{}".format(label_li[i-1]))
+    plt.show()
 
 
-        #print('ID: ' + ''.join([alphabet[i] for i in target.cpu().numpy()]) + '.jpg')
-        #print('label: '+''.join([alphabet[i] for i in output.cpu().numpy()]))
-
-        dataframe = pd.DataFrame({'ID':b,'label':a},index=[0])
-        dataframe.to_csv('1.csv',index=False,mode='a')
-
-    with open('1.csv','r') as r:
-        lines = r.readlines()
-    with open('2.csv','w') as w:
-        for i in lines:
-            if 'label' not in i:
-                w.write(i)
-
-    df= pd.read_csv('2.csv',header=None,names=['ID','label'])
-    df.to_csv('3.csv',index=False)
-
-    with open('3.csv','r') as r:
-        lines = r.readlines()
-    with open(result_path,'w') as w:
-        for l in lines:
-           str = l.strip('0')
-           w.write(str)
-
-    f = pd.read_csv(result_path)
-    print(f)
-
-    os.remove('1.csv')
-    os.remove('2.csv')
-    os.remove('3.csv')
 
 if __name__=="__main__":
-    predict()
+    run()
